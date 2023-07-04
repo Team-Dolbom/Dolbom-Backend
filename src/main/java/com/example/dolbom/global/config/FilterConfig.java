46acc2ac816603ba135aae25034e76adabc8ded1
Yukong265 <subenmoom50@gmail.com>
2023-07-04 13:26:17 +0900

    Create Various Config

---
+++ src/main/java/com/example/dolbom/global/config/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.example.dolbom.global.config;
+
+import com.example.dolbom.global.error.ExceptionHandler;
+import com.example.dolbom.global.security.jwt.JwtTokenFilter;
+import com.example.dolbom.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final ObjectMapper objectMapper;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity builder){
+        ExceptionHandler exceptionHandler = new ExceptionHandler(objectMapper);
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionHandler, JwtTokenFilter.class);
+    }
+}
---
+++ src/main/java/com/example/dolbom/global/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.dolbom.global.config;
+
+import com.example.dolbom.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final ObjectMapper objectMapper;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+
+
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().disable()
+
+                .apply(new FilterConfig(objectMapper, jwtTokenProvider));
+    }
+}
