c6b9beb18395f05295790ae45229639fd3dd33ca
Yukong265 <subenmoom50@gmail.com>
2023-07-04 11:28:37 +0900

    setting

---
+++ src/test/java/com/example/dolbom/DolbomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dolbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolbomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
