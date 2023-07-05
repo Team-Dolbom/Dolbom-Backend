a9eb32570e6615685ec8366a96889538182f9768
Yukong265 <subenmoom50@gmail.com>
2023-07-05 13:18:35 +0900

    All commit

---
+++ src/main/java/com/example/dolbom/domain/offer/present/dto/response/OfferDetailResponse.java
@@ -0,0 +1,18 @@
+package com.example.dolbom.domain.offer.present.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OfferDetailResponse {
+    private Long id;
+    private Boolean babySitter;
+    private String title;
+    private String content;
+    private String intro;
+    private String author;
+    private Boolean certification;
+}
